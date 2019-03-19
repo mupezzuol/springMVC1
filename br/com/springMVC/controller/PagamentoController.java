8f83b44f9e294aea0cdc57c38cb6499528d57cf7
mupezzuol <murillopezzuol@hotmail.com>
2019-03-18 22:41:09 -0300

    Callable (Requisicao Assincrona).

--- br/com/springMVC/controller/PagamentoController.java
+++ br/com/springMVC/controller/PagamentoController.java
@@ -1,5 +1,7 @@
 package com.springMVC.controller;
 
+import java.util.concurrent.Callable;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,18 +27,23 @@ public class PagamentoController {
 	
 	
 	@RequestMapping(value="/finalizar", method=RequestMethod.POST)
-	public ModelAndView finalizar(RedirectAttributes model) {
-		try {
-			String uri = "https://book-payment.herokuapp.com/payment";
-			String response = restTemplate.postForObject(uri, new DadosPagamento(carrinho.getTotal()), String.class);//Faz requisicao Rest e retorna
-			System.out.println(response);
-			model.addFlashAttribute("sucesso",response);//Retorno da resposta da API Rest que está sendo consumida
-			return new ModelAndView("redirect:/produtos");//Paga e retorna para listagem dos produtos
-		} catch (HttpClientErrorException e) {
-			//e.printStackTrace();//Joga o erro na tela
-			model.addFlashAttribute("falha","Valor maior que o permitido!");
-			return new ModelAndView("redirect:/produtos");
-		}
+	public Callable<ModelAndView> finalizar(RedirectAttributes model) {
+		
+		//Callable é uma classe anonima para que o Spring entenda que esse método será requisições ASSINCRONAS
+		//Por ser um classe anonima, temos que implemetar a chamada da classe anonima e colocar o código dentro dela
+		return () -> {
+			try {
+				String uri = "https://book-payment.herokuapp.com/payment";
+				String response = restTemplate.postForObject(uri, new DadosPagamento(carrinho.getTotal()), String.class);//Faz requisicao Rest e retorna
+				System.out.println(response);
+				model.addFlashAttribute("sucesso",response);//Retorno da resposta da API Rest que está sendo consumida
+				return new ModelAndView("redirect:/produtos");//Paga e retorna para listagem dos produtos
+			} catch (HttpClientErrorException e) {
+				//e.printStackTrace();//Joga o erro na tela
+				model.addFlashAttribute("falha","Valor maior que o permitido!");
+				return new ModelAndView("redirect:/produtos");
+			}
+		};
 	}
 	
 }
